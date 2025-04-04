7464bab99aa56fabf9dccf4ce2281e91a3163632
ZeyuZhao-git <zhaozeyu9185@gmail.com>
2025-04-04 14:12:43 -0400

    关注取消功能

--- src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,9 +1,13 @@
 package com.hmdp.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.UserHolder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,4 +21,29 @@ import org.springframework.stereotype.Service;
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
 
+    @Override
+    public Result isFollow(Long id) {
+        Long userId = UserHolder.getUser().getId();
+        Integer count = query().eq("user_id", userId).eq("follow_user_id", id).count();
+        return Result.ok(count > 0);
+    }
+
+    @Override
+    public Result follow(Long id, Boolean isFollow) {
+        UserDTO userDTO = UserHolder.getUser();
+        Long userId = userDTO.getId();
+        if(isFollow){
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(id);
+            save(follow);
+        }
+        else{
+            remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId)
+                    .eq("follow_user_id", id)
+            );
+        }
+        return Result.ok();
+    }
 }
