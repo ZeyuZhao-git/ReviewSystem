803a413ab455e494234311535171aa9e04615db3
ZeyuZhao-git <zhaozeyu9185@gmail.com>
2025-04-04 23:00:54 -0400

    使用GEO实现根据距离排序店铺

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,49 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
+import com.hmdp.service.IShopService;
+import com.hmdp.service.impl.ShopServiceImpl;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.geo.Point;
+import org.springframework.data.redis.connection.RedisGeoCommands;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.SHOP_GEO_KEY;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class HmDianPingApplicationTests {
+public class HmDianPingApplicationTests {
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
 
+    @Resource
+    ShopServiceImpl shopService;
+
+    @Test
+    public void loadShopData(){
+        List<Shop> list = shopService.list();
+        Map<Long, List<Shop>> map = list.stream().collect(Collectors.groupingBy(Shop::getTypeId));
+        for(Map.Entry<Long, List<Shop>> entry:map.entrySet()){
+            Long typeId = entry.getKey();
+            String key = SHOP_GEO_KEY + typeId;
+            List<Shop> shops = entry.getValue();
+            List<RedisGeoCommands.GeoLocation<String>> locations = new ArrayList<>(shops.size());
+            for(Shop shop:shops){
+                //stringRedisTemplate.opsForGeo().add(key, new Point(shop.getX(), shop.getY()), shop.getId().toString());
+                locations.add(new RedisGeoCommands.GeoLocation<>(shop.getId().toString(), new Point(shop.getX(), shop.getY())));
+            }
+            stringRedisTemplate.opsForGeo().add(key, locations);
+        }
+
+    }
 
 }
